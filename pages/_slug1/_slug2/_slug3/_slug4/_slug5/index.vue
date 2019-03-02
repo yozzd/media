77f39153599a5ed347e5344a9cbc2ba72aae819f
yozz <yozzdharma@gmail.com>
2019-03-02 08:49:45 +0700

    feat: slug5

---
+++ pages/_slug1/_slug2/_slug3/_slug4/_slug5/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Slug5 />
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+import Slug5 from '../../../../../../components/slug5/index.vue';
+
+@Component({
+  components: { Slug5 },
+})
+class Slug5Page extends Vue {}
+
+export default Slug5Page;
+</script>
