f334d0163eef4b322d1e072268e24610e72d4212
yozz <yozzdharma@gmail.com>
2019-03-06 09:23:06 +0700

    chore: update packages

--- apollo/config/default.js
+++ apollo/config/default.js
@@ -4,6 +4,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 export default (ctx) => {
   const uri = process.browser
     ? '/graphql'
+    // : 'http://190.1.1.1/graphql';
     : `${ctx.req.protocol}://${ctx.req.get('Host')}/graphql`;
 
   const link = createHttpLink({ uri });
